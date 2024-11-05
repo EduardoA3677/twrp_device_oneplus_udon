938f04bed82c4ffb59606ecdc7c005c66d4badfe
Eduardo <ealvarado261500@gmail.com>
2024-11-05 15:42:25 -0600

    Add vendor properties, update device.mk, and remove obsolete HAL services

--- device.mk
+++ device.mk
@@ -10,42 +10,87 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Configure core_64_bit_only.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Configure virtual_ab compression.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
-
 # Configure emulated_storage.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Configure twrp common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Platform
+QCOM_BOARD_PLATFORMS += $(PRODUCT_PLATFORM)
+TARGET_BOARD_PLATFORM := $(PRODUCT_PLATFORM)
+TARGET_BOOTLOADER_BOARD_NAME := $(TARGET_BOARD_PLATFORM)
+
+BUILD_BROKEN_DUP_RULES := true
+
+RELAX_USES_LIBRARY_CHECK := true
+
+# A/B support
+AB_OTA_UPDATER := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 31
+
+AB_OTA_PARTITIONS ?= boot vendor_boot recovery vendor_dlkm dtbo vbmeta super
+
+
 PRODUCT_PACKAGES += \
     bootctrl.oneplus_sm8475.recovery \
+    update_engine_client \
+    update_verifier \
     android.hardware.boot@1.2-impl-qti \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    update_engine_sideload
+
+# f2fs utilities
+PRODUCT_PACKAGES += \
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs
+
+# Userdata checkpoint
+PRODUCT_PACKAGES += \
+    checkpoint_gc
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+
+#Support to compile recovery without msm headers
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += fastbootd
 
 # Add default implementation of fastboot HAL.
+PRODUCT_PACKAGES += android.hardware.fastboot@1.1-impl-mock
+
+# qcom decryption
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    android.hardware.fastboot@1.1-impl-mock.recovery
+    qcom_decrypt \
+    qcom_decrypt_fbe
 
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+BOARD_SHIPPING_API_LEVEL := 31
+BOARD_API_LEVEL := 31
+SHIPPING_API_LEVEL := 31
+PRODUCT_SHIPPING_API_LEVEL := 31
 
-# SHIPPING API
-PRODUCT_SHIPPING_API_LEVEL := 32
+SOONG_CONFIG_NAMESPACES += ufsbsg
 
-# VNDK API
-PRODUCT_TARGET_VNDK_VERSION := 34
-
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+SOONG_CONFIG_ufsbsg += ufsframework
+SOONG_CONFIG_ufsbsg_ufsframework := bsg
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
--- twrp_udon.mk
+++ twrp_udon.mk
@@ -9,6 +9,8 @@ DEVICE_PATH := device/oneplus/udon
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
+PRODUCT_PLATFORM := taro
+
 # Release name
 PRODUCT_RELEASE_NAME := udon
 
@@ -19,8 +21,4 @@ PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := CPH2487
 PRODUCT_MANUFACTURER := OnePlus
 
-# Assert
-#TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
 
-# Theme
-TW_STATUS_ICONS_ALIGN := center
